8cf806ebd3846610b559b45a6d17ea1dca8de742
xiaoqinvar <17683860320m0@sina.cn>
2022-12-20 15:33:32 +0800

    feat: 推荐页的新碟上架样式完成

--- src/views/discover/c-views/recommend/components/new-disc/style.ts
+++ src/views/discover/c-views/recommend/components/new-disc/style.ts
@@ -1,7 +1,42 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
+  background-color: #f5f5f5;
+  border: 1px solid #d3d3d3;
+  margin: 20px 0;
+  padding: 20px 0;
+
   .ant-carousel {
-    background-color: #c7aee0;
+  }
+
+  .swiper-page {
+    height: 184px;
+    display: flex;
+    flex-wrap: nowrap;
+    flex-direction: row;
+    align-items: center;
+
+    .new-disc-item {
+      //flex: 1;
+      margin-left: 20px;
+      width: 115px;
+      position: relative;
+
+      .bg {
+        height: 100px;
+        background-size: 100px;
+        background-repeat: no-repeat;
+      }
+      p {
+        font-size: 12px;
+        cursor: pointer;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+      p.artists {
+        color: ${(props) => props.theme.color.garyColor};
+      }
+    }
   }
 `;
